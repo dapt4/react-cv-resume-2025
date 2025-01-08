5061afb641e5ff7f7a27817766c7888bf56f234c
dapt <diegodapt18@gmail.com>
2025-01-08 23:48:49 +0100

    subiendo archivos

---
+++ src/Schemas/Data.js
@@ -0,0 +1,159 @@
+export const Data = {
+  profile: {
+    name: "Diego Perozo",
+    ocupation: "Mobile FullStack Developer",
+    location: "Spain",
+    email: "diegodapt18@gmail.com",
+    telephone: "+34 643891809",
+    image: "images/me.jpeg",
+  },
+  aboutMe: {
+    label: "Profile",
+    description: `Mobile application development, integrating frontend and backend to
+    create complete and functional solutions. Experienced in implementing 
+    intuitive interfaces and API connectivity.`,
+  },
+  skills: {
+    technicalLabel: "Technologies",
+    softLabel: "Skills",
+    technicalSkills: [
+      "Angular",
+      "Ionic",
+      "Cordova",
+      "typescript",
+      "javascript",
+      "nopdejs",
+      "express",
+      "React",
+      "vuejs",
+      "sass",
+      "python",
+      "django",
+      "SQL",
+      "postgreSQL",
+      "MySQL",
+      "MariaDB",
+      "eslint",
+      "github",
+      "Git",
+    ],
+    softSkills: [
+      "Analytical skills",
+      "Problem solving",
+      "Critical thinking",
+      "Teamwork",
+      "Communication",
+      "Proactivity",
+      "Creativity",
+      "Self-learning",
+      "Mentoring",
+    ],
+  },
+  socialMedia: {
+    label: "SOCIAL",
+    social: [
+      {
+        label: "Visit Diego Perozo's profile on LinkedIn",
+        name: "linkedin",
+        url: "https://www.linkedin.com/in/diegoperozotroncone",
+        className: "bxl-linkedin-square",
+      },
+      {
+        label: "Visit Diego Perozo's profile on GitHub",
+        name: "github",
+        url: "https://github.com/dapt4",
+        className: "bxl-github",
+      },
+    ],
+  },
+  experience: {
+    works: [
+      {
+        title: "Mobile FullStack Developer",
+        period: "May 2024 - Present",
+        company: "Asesoría y Medios de Gestión S.L.",
+        description: [
+          `Mobile application development with Ionic, Cordova, and Angular.
+          Backend development based on SQL, implementation of best practices, 
+          and creation of components and services to be shared across applications, 
+          solving issues related to disparity between them.`,
+          `Technologies used include Ionic, Cordova, Angular 15, Sass, Node.js,
+          Express, MariaDB, and SQL.`,
+        ],
+      },
+      {
+        title: "Fullstack developer",
+        period: "Dec 2023 - May 2024",
+        company: "With madrid S.L.",
+        description: [
+          `Frontend and backend development for a banking institution. 
+          The work primarily involved creating models, endpoints, and 
+          integrating them with the UI, focusing on data security and 
+          adding new features for optimization.`,
+          `Technologies used include Python, Django, Wagtail, Nuxt, Vue.js, 
+          TypeScript, Sass, and Vuex.`,
+        ],
+      },
+      {
+        title: "Frontend developer",
+        period: "Jan 2020 - Dec 2023",
+        company: "Argotec S.L.",
+        description: [
+          `We developed an agriculture application from scratch featuring maps 
+          and charts. Reusable and functional components were created to 
+          standardize the app and enhance usability. The app is used by 
+          thousands of users worldwide.`,
+          `Technologies used include TypeScript, Sass, Highcharts,
+          and Leaflet.`,
+        ],
+      },
+      {
+        title: "Frontend developer",
+        period: "Jan 2019 - Dec 2019",
+        company: "Hellosabi inc.",
+        description: [
+          `I developed frontend features for a consumer sales application and 
+          proposed new components to optimize the user experience.`,
+          `Technologies used include React with TypeScript, Redux, and Styled 
+          Components.`,
+        ],
+      },
+    ],
+    academic: [
+      {
+        career: "Backend development with python Django, flask, fastAPI",
+        date: "2017",
+        institution: "https://platzi.com/ruta/backend-python/",
+      },
+      {
+        career: "Bachelor of Graphic Design",
+        date: "2014",
+        institution: "University Rafael Belloso Chacín",
+      },
+    ],
+    proyects: [
+      {
+        name: "Development of a web application for a banking institution",
+        company: "CA Mon Commerce - https://www.ca-moncommerce.com",
+        period: "Dec 2023 - May 2024",
+        description: [
+          `Development of a web application for scheduling appointments with the
+          public, featuring user registration, calendar management, and automated 
+          notifications. The application prioritizes user experience and security, 
+          using modern web technologies.`,
+        ],
+      },
+      {
+        name: "Development of a web application for an agriculture-based platform",
+        company: "Argotec S.L. - https://qampo.es",
+        period: "Jan 2020 - Dec 2023",
+        description: [
+          `Development of a web application for agronomy, focusing on managing 
+          agricultural data, crop monitoring, and resource optimization. 
+          The application provides data-driven insights and a user-friendly 
+          interface for agricultural professionals.`,
+        ],
+      },
+    ],
+  },
+};
